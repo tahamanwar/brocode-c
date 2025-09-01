567ec30428114597ec8f21c9696af92b15e7e05c
Taha Anwar <tul26954@temple.edu>
2025-09-01 17:51:24 -0400

    Initial commit: Hello World + My Name

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+/* My first program, "Hello World" */ 
+
+#include <stdio.h> 
+
+int main(void)
+{
+    printf("Hello World! \n");
+}
---
+++ myname.c
@@ -0,0 +1,9 @@
+/* Program to print "Hello, my name is Taha" */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+    printf("Hello, my name is Taha \n");
+}
